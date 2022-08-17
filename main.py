c78f8fc512e9cd864a2e3b81fed4401257281243
N3xTr1Me <54271030+N3xTr1Me@users.noreply.github.com>
2022-08-17 20:41:14 +0300

    new ver

--- main.py
+++ main.py
@@ -1,32 +1,46 @@
 from math import pi, exp
 from random import randint
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from copy import deepcopy
+from numba import njit
 
-A = 10
-B = 10
-max_temperature = 10
+import datetime
 
-a = [[0] * A for _ in range(B)]
 
-for i in range(B):
-    for j in range(A):
-        a[i][j] = randint(1, max_temperature)
-
-new_a = [[0] * A for _ in range(B)]
-
-for t in range(1, 30):
-    t /= 2
-    for i in range(B):
-        for j in range(A):
-            T = [[0] * A for _ in range(B)]
-            for x1 in range(B):
-                for x2 in range(A):
+@njit
+def solve(a, t):
+    new_a = np.zeros((width, height))
+    for i in range(height):
+        for j in range(width):
+            T = [[0] * width for _ in range(height)]
+            for x1 in range(height):
+                for x2 in range(width):
                     T[x1][x2] = a[x1][x2] * exp(-1 / 4 / t * ((i - x1) * 2 + (j - x2) * 2))
             s = sum([sum(i) for i in T]) / 4 / pi / t
             new_a[i][j] = s
-    a = deepcopy(new_a)
+    return new_a
+
+
+width = 30
+height = 30
+
+min_temperature = 0
+max_temperature = 10
+
+a = np.zeros((width, height))
+
+for i in range(height):
+    for j in range(width):
+        a[i][j] = randint(min_temperature, max_temperature)
+
+t1 = datetime.datetime.now()
+
+for i in range(1, 30):
+    a = solve(a, i / 10)
     sns.color_palette("magma", as_cmap=True)
-    sns.heatmap(a, vmin=0, vmax=max_temperature)
+    sns.heatmap(a, vmin=min_temperature, vmax=max_temperature)
     plt.show()
+
+print(datetime.datetime.now() - t1)
