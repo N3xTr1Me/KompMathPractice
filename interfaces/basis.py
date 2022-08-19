89e0215f9071f379e43d1d70bb7ef7bc02747632
N3xTr1Me <54271030+N3xTr1Me@users.noreply.github.com>
2022-08-19 18:41:45 +0300

    Basis fix

--- interfaces/basis.py
+++ interfaces/basis.py
@@ -5,17 +5,22 @@ from typing import Dict
 
 class IBasis(ABC):
 
+    @abstractmethod
     def initialize_basis(self, basis_functions: Dict[callable]) -> None:
         pass
 
+    @abstractmethod
     def phi_1(self) -> callable:
         pass
 
+    @abstractmethod
     def phi_2(self) -> callable:
         pass
 
+    @abstractmethod
     def d_phi_1(self) -> callable:
         pass
 
+    @abstractmethod
     def d_phi_2(self) -> callable:
         pass
