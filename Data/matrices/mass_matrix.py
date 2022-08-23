d9c0f2bce681c4bdb41d07a674d48ba1457582c7
N3xTr1Me <54271030+N3xTr1Me@users.noreply.github.com>
2022-08-23 05:03:27 +0300

    Mass matrix fix

--- Data/matrices/mass_matrix.py
+++ Data/matrices/mass_matrix.py
@@ -5,14 +5,15 @@ from Data.field import Field
 
 
 class MassMatrix(Matrix):
-    def __init__(self, source: Field):
+    def __init__(self, source: Field, redefine: bool = False):
 
         rows, columns = source.dimensions()
         super(MassMatrix, self).__init__(rows=rows, columns=columns)
 
         self._source = source
 
-        self.fill()
+        if not redefine:
+            self.fill()
 
     def _get_from_src(self, row: int, index: int) -> np.array:
         return self._source.get_mass(row, index)
@@ -27,7 +28,6 @@ class MassMatrix(Matrix):
             row = Matrix(data.shape[0], data.shape[1], data)
 
             for j in range(1, columns):
-                print(i, j)
                 row.merge(self._get_from_src(i, j), axis=1)
 
             if i == 0:
