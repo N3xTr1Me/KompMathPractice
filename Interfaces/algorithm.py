4416d7ad1f7ab083768ca0141774afa59ef94f3d
N3xTr1Me <destriker17@gmail.com>
2022-09-02 02:19:35 +0300

    Added algorithm class interface

---
+++ Interfaces/algorithm.py
@@ -0,0 +1,34 @@
+from abc import ABC, abstractmethod
+
+from Data.matrices.matrix import Matrix
+from Data.matrices.mass_matrix import MassMatrix
+from Data.matrices.stiffness_matrix import StiffnessMatrix
+
+from Model.time_frame import Frame
+
+
+class IAlgorithm(ABC):
+
+    @abstractmethod
+    def __mass(self) -> MassMatrix:
+        pass
+
+    @abstractmethod
+    def __stiffness(self) -> StiffnessMatrix:
+        pass
+
+    @abstractmethod
+    def __b(self) -> Matrix:
+        pass
+
+    @abstractmethod
+    def __k(self, current: Frame, t: float) -> float:
+        pass
+
+    @abstractmethod
+    def __build_frame(self, xi: Matrix, t: float) -> Frame:
+        pass
+
+    @abstractmethod
+    def step(self, t: float, current: Frame, previous: Frame) -> Frame:
+        pass
