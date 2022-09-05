f90ce54469649cb4347b99505db936af2ef595f7
N3xTr1Me <destriker17@gmail.com>
2022-09-05 03:21:32 +0300

    Storage functionality implementation for Model

--- Model/model.py
+++ Model/model.py
@@ -2,12 +2,18 @@ from typing import Tuple
 
 from Data.basis import Basis
 from Model.fem import FEM
+from Model.file_handler import Storage
 
 from Model.frame import Frame
 
 
 class Model:
     def __init__(self, T: int, step: float, dimensions: Tuple[int, int], right_side: callable):
+        self.__T = T
+        self.__t = step
+        self.__step = 0
+
+        # --------------------------------------------------------------------------------------------------------------
 
         self.__basis = Basis({"phi_1": lambda x, y, w, h: (x + w) - (4 * y - h) + 1,
                               "phi_2": lambda x, y, w, h: (2 * x - w) + (y + h) - 5,
@@ -15,23 +21,39 @@ class Model:
                               "d_phi_2": lambda x, y, w, h: 2
                               })
 
-        self.__T = T
-        self.__step = step
-
         self.__solver = FEM(dimensions, self.__basis, right_side)
 
-        self.__cache = []
+        # --------------------------------------------------------------------------------------------------------------
 
-        self.__solution = {"dimensions": dimensions,
-                           "basis": self.__basis,
-                           "coefficients": None
-                           }
+        cache_path = "../Cache/data/"
+        config_path = "../Config/config.json"
 
-    def __current(self):
-        return self.__cache[-1]
+        self.__storage = Storage(config_path, cache_path)
+        self.__config = self.__storage.get_config()
 
-    def __previous(self):
-        return self.__cache[-2]
+        self.__session = ""
+        self.__cached = []
 
-    def __cache(self, value: Frame):
-        self.__cache.append(value)
+    def __current(self) -> Frame:
+        return self.__storage.get_step(self.__session, self.__cache[-1])
+
+    def __previous(self) -> Frame:
+        return self.__storage.get_step(self.__session, self.__cache[-2])
+
+    def __cache(self, step: Frame) -> None:
+        self.__cached.append(step.t())
+
+        self.__storage.store(self.__session, step)
+
+    def get(self, step: float) -> Frame:
+        return self.__storage.get_step(self.__session, step)
+
+    def make_step(self) -> Frame:
+        iteration = self.__solver.step(self.__t, self.__current(), self.__previous())
+
+        self.__step += self.__t
+
+        iteration.set_temperatures(self.__basis)
+        self.__cache(iteration)
+        return iteration
+
