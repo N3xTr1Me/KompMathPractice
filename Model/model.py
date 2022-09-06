0e9bbf1569ada7a4629962f1028448dffaaad163
N3xTr1Me <destriker17@gmail.com>
2022-09-06 19:04:44 +0300

    Model semi-done

--- Model/model.py
+++ Model/model.py
@@ -1,6 +1,10 @@
-from typing import Tuple
+import random
+from typing import Tuple, Any
 
 from Data.basis import Basis
+from Model.right_side.functor import Functor
+from Model.right_side.table import Table
+
 from Model.fem import FEM
 from Model.file_handler import Storage
 
@@ -8,20 +12,14 @@ from Model.frame import Frame
 
 
 class Model:
-    def __init__(self, T: int, step: float, dimensions: Tuple[int, int], right_side: callable):
+    def __init__(self, T: int, step: float, dimensions: Tuple[int, int], right_side: callable = None):
+
+        # right border of I (time interval)
         self.__T = T
+        # time step of I
         self.__t = step
+        # current step
         self.__step = 0
-
-        # --------------------------------------------------------------------------------------------------------------
-
-        self.__basis = Basis({"phi_1": lambda x, y, w, h: (x + w) - (4 * y - h) + 1,
-                              "phi_2": lambda x, y, w, h: (2 * x - w) + (y + h) - 5,
-                              "d_phi_1": lambda x, y, w, h: 1,
-                              "d_phi_2": lambda x, y, w, h: 2
-                              })
-
-        self.__solver = FEM(dimensions, self.__basis, right_side)
 
         # --------------------------------------------------------------------------------------------------------------
 
@@ -31,14 +29,35 @@ class Model:
         self.__storage = Storage(config_path, cache_path)
         self.__config = self.__storage.get_config()
 
+        if right_side is None:
+            self.__right_side = Table(dimensions[0], dimensions[1], self.__config["right-side"])
+        else:
+            self.__right_side = Functor(dimensions[0], dimensions[1], right_side)
+
+        # --------------------------------------------------------------------------------------------------------------
+
+        self.__basis = Basis({"phi_1": lambda x, y, w, h: (x + w) - (4 * y - h) + 1,
+                              "phi_2": lambda x, y, w, h: (2 * x - w) + (y + h) - 5,
+                              "d_phi_1": lambda x, y, w, h: 1,
+                              "d_phi_2": lambda x, y, w, h: 2
+                              })
+
+        self.__solver = FEM(dimensions, self.__basis, self.__right_side)
+
         self.__session = ""
         self.__cached = []
 
     def __current(self) -> Frame:
-        return self.__storage.get_step(self.__session, self.__cache[-1])
+        return self.__storage.get_step(self.__session, self.__cached[-1])
 
-    def __previous(self) -> Frame:
-        return self.__storage.get_step(self.__session, self.__cache[-2])
+    def __previous(self) -> Any | None:
+        if self.__cached:
+            return self.__cached[-1]
+        else:
+            return None
+
+    def __start_calculation(self):
+        self.__session = self.__storage.start_session()
 
     def __cache(self, step: Frame) -> None:
         self.__cached.append(step.t())
@@ -49,7 +68,11 @@ class Model:
         return self.__storage.get_step(self.__session, step)
 
     def make_step(self) -> Frame:
-        iteration = self.__solver.step(self.__t, self.__current(), self.__previous())
+
+        if not self.__session:
+            self.__start_calculation()
+
+        iteration = self.__solver.step(self.__t, self.__previous())
 
         self.__step += self.__t
 
@@ -57,3 +80,18 @@ class Model:
         self.__cache(iteration)
         return iteration
 
+    def run_algorithm(self):
+
+        if not self.__session:
+            self.__start_calculation()
+
+        while self.__step < self.__T:
+            iteration = self.make_step()
+
+
+dims = (10, 10)
+time = 10
+time_step = 1
+
+model = Model(T=time, step=time_step, dimensions=dims, right_side=lambda x, y: random.randint(1, 10))
+model.run_algorithm()
