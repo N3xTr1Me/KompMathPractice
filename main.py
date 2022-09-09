80d2cbbd8ac85e8865abf4078bf8e1af98f52b03
Philipoff <Filippok2002@yandex.ru>
2022-09-09 13:59:58 +0300

    update README.md

--- main.py
+++ main.py
@@ -1,46 +1,17 @@
-from math import pi, exp
-from random import randint
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-from copy import deepcopy
-from numba import njit
-
 import datetime
+import random
+
+from Model.model import Model
 
 
-@njit
-def solve(a, t):
-    new_a = np.zeros((width, height))
-    for i in range(height):
-        for j in range(width):
-            T = [[0] * width for _ in range(height)]
-            for x1 in range(height):
-                for x2 in range(width):
-                    T[x1][x2] = a[x1][x2] * exp(-1 / 4 / t * ((i - x1) ** 2 + (j - x2) ** 2))
-            s = sum([sum(i) for i in T]) / 4 / pi / t
-            new_a[i][j] = s
-    return new_a
+dims = (10, 10)
+time = 10
+time_step = 0.5
 
+start_1 = datetime.datetime.now()
+m = Model(T=time, step=time_step, dimensions=dims, right_side=lambda x, y: random.randint(1, 10))
 
-width = 30
-height = 30
-
-min_temperature = 0
-max_temperature = 10
-
-a = np.zeros((width, height))
-
-for i in range(height):
-    for j in range(width):
-        a[i][j] = randint(min_temperature, max_temperature)
-
-t1 = datetime.datetime.now()
-
-for i in range(1, 30):
-    a = solve(a, i / 10)
-    sns.color_palette("magma", as_cmap=True)
-    sns.heatmap(a, vmin=min_temperature, vmax=max_temperature)
-    plt.show()
-
-print(datetime.datetime.now() - t1)
+start_2 = datetime.datetime.now()
+m.run_algorithm()
+print(datetime.datetime.now() - start_2)
+print(datetime.datetime.now() - start_1)
