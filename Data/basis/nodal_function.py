17d875dc4872cf1729e76a65b3ad136b8b5cff8a
N3xTr1Me <destriker17@gmail.com>
2022-09-20 21:40:19 +0300

    Added nodal basis classes

---
+++ Data/basis/nodal_function.py
@@ -0,0 +1,33 @@
+from Interfaces.basis.nodal_basis import INodal
+
+from typing import Dict
+
+
+# Nodal basis function
+class Phi(INodal):
+    def __init__(self, constants: Dict[str, float]):
+        required = ["a", "b", "c"]
+
+        for arg in required:
+            if arg not in constants:
+                raise ValueError(f"{arg} not found in constants!")
+
+        self.__constants = constants
+
+    def a(self) -> float:
+        return self.__constants["a"]
+
+    def b(self) -> float:
+        return self.__constants["b"]
+
+    def c(self) -> float:
+        return self.__constants["c"]
+
+    def f(self) -> callable:
+        return lambda x, y: self.a() * x + self.b() * y + self.c()
+
+    def __call__(self, x: float, y: float) -> float:
+        return self.f()(x, y)
+
+    def __str__(self):
+        return f"({self.a()})x + ({self.b()})y + ({self.c()})"
