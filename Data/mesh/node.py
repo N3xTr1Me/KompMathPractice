21f5323999f7e84130f08613a50ccac64798ec18
N3xTr1Me <destriker17@gmail.com>
2022-09-21 02:43:32 +0300

    Node class adjustments

--- Data/mesh/node.py
+++ Data/mesh/node.py
@@ -1,10 +1,13 @@
+from Interfaces.mesh.node_interface import INode
+
 from Data.basis.basis import Basis
 
 from typing import Tuple
 
 
-# a node-dot on the 2D grid. Has (x,y) coordinates and a set of basis functions, holds 2 neighboring node's coordinates
-class Node:
+# a node-dot on the 2D grid. Has (x,y) coordinates, a set of basis functions and nodal value, holds 2 neighboring
+# node's coordinates
+class Node(INode):
     def __init__(self, i: Tuple[float, float], j: Tuple[float, float], k: Tuple[float, float]):
         super(Node, self).__init__()
 
@@ -15,8 +18,13 @@ class Node:
         self.__j = j
         self.__k = k
 
-        # initializing node's basis functions
+        # initializing node's basis functions, consisting of
+        # the original function: ax + by + c,
+        # and it's first derivative
         self.__basis = Basis(j, k)
+
+        # Nodal value
+        self.__u = None
 
     # ----------------------------------------------------------------------------------------------------------------------
 
@@ -29,25 +37,45 @@ class Node:
     def coords(self) -> Tuple[float, float]:
         return self.__i
 
+    # Returns "left" neighbours coordinates
     def j(self) -> Tuple[float, float]:
         return self.__j
 
+    # Returns "right" neighbours coordinates
     def k(self) -> Tuple[float, float]:
         return self.__k
 
     # ------------------------------------------------------------------------------------------------------------------
 
-    def basis(self) -> Basis:
-        return self.__basis
-
+    # Returns "a" constant from nodal basis function
     def a(self) -> float:
         return self.__basis.get_a()
 
+    # Returns "b" constant from nodal basis function
     def b(self) -> float:
         return self.__basis.get_b()
 
+    # Returns "c" constant from nodal basis function
     def c(self) -> float:
         return self.__basis.get_c()
+
+    # ------------------------------------------------------------------------------------------------------------------
+
+    # Returns nodal value
+    def u(self) -> float:
+        return self.__u
+
+    # Sets nodal value
+    def set_u(self, value: float) -> None:
+        self.__u = value
+
+    # Returns nodal basis function value from given arguments
+    def phi(self, x: float, y: float) -> float:
+        return self.__basis.phi()(x, y)
+
+    # Returns nodal basis first derivative value from given arguments
+    def d_phi(self, x: float, y: float) -> float:
+        return self.__basis.d_phi()(x, y)
 
     # ----------------------------------------------------------------------------------------------------------------------
 
