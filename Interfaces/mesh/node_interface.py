32a733c7f35216bf9001f8f0f85c86d6d115abe3
N3xTr1Me <destriker17@gmail.com>
2022-09-22 01:52:06 +0300

    Node class overhaul

--- Interfaces/mesh/node_interface.py
+++ Interfaces/mesh/node_interface.py
@@ -1,42 +1,12 @@
 from abc import ABC, abstractmethod
 
-from typing import Tuple
-
 
 class INode(ABC):
-
-    @abstractmethod
-    def coords(self) -> Tuple[float, float]:
-        pass
-
-    @abstractmethod
-    def set_neighbours(self, j, k) -> None:
-        pass
-
-    @abstractmethod
-    def i(self):
-        pass
-
-    @abstractmethod
-    def j(self):
-        pass
 
     @abstractmethod
     def u(self) -> float:
         pass
 
     @abstractmethod
-    def set_u(self, value: float) -> None:
-        pass
-
-    @abstractmethod
-    def set_basis(self, j: Tuple[float, float], k: Tuple[float, float]):
-        pass
-
-    @abstractmethod
-    def phi(self, x: float, y: float) -> float:
-        pass
-
-    @abstractmethod
-    def d_phi(self, x: float, y: float) -> float:
+    def update(self, value: float) -> None:
         pass
