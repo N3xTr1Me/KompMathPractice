54c42f42a1cab4dc0e08fab85cc4d045dea298e9
N3xTr1Me <destriker17@gmail.com>
2022-09-22 00:31:44 +0300

    Model adjustments

--- Model/algorithm/fem.py
+++ Model/algorithm/fem.py
@@ -8,7 +8,6 @@ from Data.matrices.matrix import Matrix
 from Data.matrices.mass_matrix import MassMatrix
 from Data.matrices.stiffness_matrix import StiffnessMatrix
 
-from Data.basis.basis import Basis
 from Data.mesh.domain import Domain
 from Model.algorithm.frame import Frame
 
@@ -16,8 +15,9 @@ import numpy as np
 
 
 class FEM(IAlgorithm):
-    def __init__(self, dimensions: Tuple[int, int], basis: Basis, right_side: callable):
+    def __init__(self, dimensions: Tuple[int, int], right_side: callable):
         self.__domain = Domain(dimensions[0], dimensions[1], right_side)
+        self.__f = right_side
 
     # Mass matrix
     def _mass(self) -> MassMatrix:
@@ -34,41 +34,49 @@ class FEM(IAlgorithm):
                                "xi": xi.get_data()})
 
     # b_n
-    def _b(self) -> Matrix:
+    def _b(self, current: float, previous: float) -> Matrix:
         # TODO: implement b_n calculation from the guidebook
 
-        return Matrix(self.__domain.rows() * 2, self.__domain.columns() * 2,
-                      np.zeros((self.__domain.rows() * 2, self.__domain.columns() * 2)))
+        rows = self.__domain.get_height()
+
+        b_vector = np.zeros((rows, 1))
+
+        for y in range(rows):
+            b_vector[y][0] = 0
+
+        return Matrix(rows, 1, b_vector)
 
     def _k(self, current: Frame, t: float) -> float:
         return current.t() - t
 
     def _ksi_n(self, left_side: Matrix, right_side: Matrix) -> Matrix:
 
-        first_multiplier = inv(left_side.get_data())
-        return Matrix(right_side.rows(), right_side.columns(),
-                      data=first_multiplier * right_side.get_data())
+        left_side.update_data(inv(left_side.get_data()))
+        ksi = left_side * right_side.get_data()
+
+        return ksi
+
+    def _ksi_0(self) -> Matrix:
+
 
     # Performs a step of algorithm
     def step(self, t: float, previous: Frame = None) -> Frame:
 
         M = self._mass()
         S = self._stiffness()
-        b = self._b()
-        b = Matrix(self.__domain.rows() * 2, self.__domain.columns() * 2,
-                   np.zeros((self.__domain.rows() * 2, self.__domain.columns() * 2)))
-        b.change_value(5, 5, 13)
 
         if previous is not None:
             k = t
             step = previous.t() + t
-            right_side = Matrix(self.__domain.rows() * 2, self.__domain.columns() * 2,
+            b = self._b(step, previous.t())
+            right_side = Matrix(self.__domain.get_height(), self.__domain.get_width(),
                                 previous.M() * previous.Xi() + b.get_data())
         else:
+            b = self._b(t, 0)
             k = 0
             step = 0
-            right_side = Matrix(self.__domain.rows() * 2, self.__domain.columns() * 2,
-                                np.zeros((self.__domain.rows() * 2, self.__domain.columns() * 2)))
+            right_side = Matrix(self.__domain.get_height(), 1,
+                                np.zeros((self.__domain.get_height(), 1)))
 
         left_side = M + S * k
 
--- main.py
+++ main.py
@@ -4,15 +4,15 @@ import random
 from Model.model import Model
 from Graphics.graphics import Graphics
 
-
 dims = (10, 10)
-time = 10
-time_step = 0.5
+time = 0.2
+time_step = 0.001
 steps_count = int(time / time_step)
 
 model = Model(T=time, step=time_step, dimensions=dims, right_side=lambda x, y: random.randint(1, 10))
 model.run_algorithm()
 
-graphics = Graphics(steps_count, time_step, model)
-graphics.draw()
+# graphics = Graphics(steps_count, time_step, model)
+# graphics.draw()
 
+print()
